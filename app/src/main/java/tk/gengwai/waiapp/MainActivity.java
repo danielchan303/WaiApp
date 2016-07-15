5869d3c8772b8338f09fdc39035e798e60779879
Daniel Chan <danielchan303@gmail.com>
2016-07-15 17:26:19 +0800

    Now the gengwai.tk land on the phone log!

--- app/src/main/java/tk/gengwai/waiapp/MainActivity.java
+++ app/src/main/java/tk/gengwai/waiapp/MainActivity.java
@@ -1,15 +1,27 @@
 package tk.gengwai.waiapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -18,17 +30,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getComment();
-
         //  Set the OnClickListener to the waiImg
         ImageView waiImg = (ImageView) findViewById(R.id.waiImg);
         waiImg.setOnClickListener(new rightAns());
-
         // Set the OnClickListener to the tigerImg
         ImageView tigerImg = (ImageView) findViewById(R.id.tigerImg);
         tigerImg.setOnClickListener(new wrongAns());
-
         // Set the OnClickListener to the tortoriseImg
         ImageView tortoiseImg = (ImageView) findViewById(R.id.tortoiseImg);
         tortoiseImg.setOnClickListener(new wrongAns());
@@ -51,41 +58,114 @@ public class MainActivity extends AppCompatActivity {
             reset();
         }
     }
-
     public void reset() {
         ImageView tigerImg = (ImageView) findViewById(R.id.tigerImg);
         tigerImg.setVisibility(View.VISIBLE);
         ImageView tortoiseImg = (ImageView) findViewById(R.id.tortoiseImg);
         tortoiseImg.setVisibility(View.VISIBLE);
-
     }
 
-    private void getComment() {
+    private class getComment extends AsyncTask<Void, Void, String> {
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
-
         // Will contain the raw JSON response as a string.
         String forecastJsonStr = null;
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                return downloadUrl();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("Async Test", result);
+        }
+    }
 
+    private String downloadUrl() throws IOException {
+        InputStream is = null;
+        URL url = new URL ("http://www.gengwai.tk/php/vid-action.php");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            URL url = new URL("http://www.gengwai.tk/php/vid-action.php");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Start the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("DEBUG_TAG", "The response is" + response);
+            is = conn.getInputStream();
 
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-        } catch (IOException e) {
-            Log.e("PlaceholderFragment", "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attemping
-            // to parse it.
+            // Convert the InputStream into a sstring
+            String contentAsString = readIt(is, 45);
+            return contentAsString;
         } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
+            if (is != null) {
+                is.close();
+            }
+            if (conn != null) {
+                conn.disconnect();
             }
         }
     }
 
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    public boolean checkInternetConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // fetch data
+            return true;
+        } else {
+            // display error
+            return false;
+        }
+    }
+
+
+    // Inflate the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // Menu Item Click Actions
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case (R.id.aboutAuthor):
+                Toast.makeText(this, "Prouldy created by Daniel Chan", Toast.LENGTH_SHORT).show();
+                if (checkInternetConnected()) {
+                    Log.v("Network Status", "Connected to the Internet");
+                    new getComment().execute();
+                } else {
+                    Log.v("Network Status", "Connection Error.");
+                }
+                return true;
+            case (R.id.people):
+                Intent callPeopleActivity = new Intent(MainActivity.this, People.class);
+                this.startActivity(callPeopleActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
---
+++ app/src/main/java/tk/gengwai/waiapp/People.java
@@ -0,0 +1,13 @@
+package tk.gengwai.waiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class People extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people);
+    }
+}
